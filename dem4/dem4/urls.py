473d9f8d9a3601e8b66b040995df243a3b34c6b3
unknown <demchakromka@gmail.com>
2025-08-19 11:38:32 +0300

    FIX rec

--- dem4/dem4/urls.py
+++ dem4/dem4/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('start.urls', namespace='start')),
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
 ]
