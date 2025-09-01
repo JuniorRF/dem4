6483dd3a80b3a67f692a1f38928723ce5f03f365
unknown <demchakromka@gmail.com>
2025-09-01 18:17:10 +0300

    CSRF_TRUSTED_ORIGINS

--- dem4/dem4/settings.py
+++ dem4/dem4/settings.py
@@ -123,4 +123,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 STATIC_ROOT = BASE_DIR / 'collected_static'
 
-SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+# SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+CSRF_TRUSTED_ORIGINS = [
+    'https://xn----7sblecanvqf5b6eve.xn--p1ai'
+]
