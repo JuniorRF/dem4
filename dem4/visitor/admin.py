f5e86c3a3b568aacfcd865e2a5af458dc87500d2
unknown <demchakromka@gmail.com>
2025-11-26 16:47:13 +0300

    ip fix

--- dem4/visitor/admin.py
+++ dem4/visitor/admin.py
@@ -4,5 +4,6 @@ from .models import Visitor
 @admin.register(Visitor)
 class VisitorAdmin(admin.ModelAdmin):
     list_display = ('user','ip_address', 'path', 'visit_time')
+    readonly_fields = ('user','ip_address', 'path', 'visit_time', 'referer', 'user_agent')
     search_fields = ('ip_address', 'path')
     list_filter = ('ip_address', 'visit_time' )
